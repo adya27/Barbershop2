79804ee2ecee13619bd8664bff95b9f06db395f6
adya27 <31897074+adya27@users.noreply.github.com>
2020-07-09 19:36:47 +0300

    Updates

--- src.8b1a1368.js
+++ src.8b1a1368.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/parcel-project-template/src.8b1a1368.js.map
+//# sourceMappingURL=/Barbershop2/src.8b1a1368.js.map
